6e0549d2c42fb06b9db56d4a0fd77ec8ec719c42
Mikhail Sol <solomennikov_m@mail.ru>
2025-10-07 14:44:55 +0500

    new project

---
+++ catalog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home_page(request):
+    return render(request, 'catalog/home.html')
+
+def contacts_page(request):
+    return render(request, 'catalog/contacts.html')
